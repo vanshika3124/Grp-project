8bb1ac8d0f9da89b05bfd07e4643b130eb60a3da
Vanshika Agrawal <vanshika310.ghaziabad@gmail.com>
2025-10-23 22:19:46 +0530

    solving error

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import './App.css';
 
 // Import all your components
 import Navbar from './components/Navbar';
-import Hero from './components/Hero';
-import InfoCards from './components/InfoCards';
-import StartTeaching from './components/StartTeaching';
-import HowToTeach from './components/HowToTeach';
-import Reviews from './components/Reviews';
-import FAQs from './components/FAQs';
-import Benefits from './components/Benefits';
-import Stats from './components/Stats';
-import Footer from './components/Footer';
+// import Hero from './components/Hero';
+// import InfoCards from './components/InfoCards';
+// import StartTeaching from './components/StartTeaching';
+// import HowToTeach from './components/HowToTeach';
+// import Reviews from './components/Reviews';
+// import FAQs from './components/FAQs';
+// import Benefits from './components/Benefits';
+// import Stats from './components/Stats';
+// import Footer from './components/Footer';
 
 function App() {
   return (
